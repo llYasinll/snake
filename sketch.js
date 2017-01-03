0cef31464171ebf178fe06289e55271cc0b98985
llYasinll <lloyasinll@gmail.com>
2017-01-03 14:34:41 -0700

    snake's head is showing and moving

--- sketch.js
+++ sketch.js
@@ -1,8 +1,32 @@
+var cols;
+var rows;
+var snake;
+var food;
+var scl = 20;
+
 function setup() {
     createCanvas(800, 600);
+    rows = floor(height / scl);
+    cols = floor(width / scl);
+    snake = new Snake();
+    frameRate(10);
 }
 
 function draw() {
-  background(51);
+    background(51);
+    fill(255);
+    snake.show();
+    snake.update();
+}
 
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        snake.dir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.dir(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.dir(1, 0);
+    } else if (keyCode === LEFT_ARROW) {
+        snake.dir(-1, 0);
+    }
 }
--- snake.js
+++ snake.js
@@ -1,5 +1,28 @@
-
-
 function Snake() {
-  
+    this.x = floor(random(cols)) * scl;
+    this.y = floor(random(rows)) * scl;
+    this.xspeed = 1;
+    this.yspeed = 0;
+
+    this.show = function() {
+        fill(255);
+        rect(this.x, this.y, scl, scl);
+    }
+
+
+    this.update = function () {
+      this.x += this.xspeed * scl;
+      this.y += this.yspeed * scl;
+
+      this.x = constrain(this.x, 0, width - scl);
+      this.y = constrain(this.y, 0, height - scl);
+    }
+
+    this.dir = function(x, y) {
+
+        if(!(this.xspeed * -1 ==x || this.yspeed * -1 == y)){
+        this.xspeed = x;
+        this.yspeed = y;
+      }
+    }
 }
